73da90db3fe4fc8e71ecc90cd6e938302bd6079a
Thiago Machado Pedro <94803123+ThiagoMachadoPedro@users.noreply.github.com>
2023-11-27 00:15:13 -0300

    feita somatoria dos campo

---
+++ main.js
@@ -0,0 +1,18 @@
+function calcularSoma() {
+  // Obter os valores dos campos de entrada
+  var valores = [];
+  for (var i = 1; i <= 12; i++) {
+      var campo = document.getElementById('valor' + i);
+      if (campo.value !== "") {
+          valores.push(parseFloat(campo.value));
+      }
+  }
+
+  // Calcular a soma
+  var soma = valores.reduce(function (total, valor) {
+      return total + valor;
+  }, 0);
+
+  // Exibir o resultado
+  document.getElementById('resultado').innerText = soma;
+}
--- scrip.js
+++ scrip.js
@@ -1,5 +1,6 @@
 const btnGenerate = document.querySelector("#gerador-pdf")
 
+
 btnGenerate.addEventListener("click", () => {
 
   const content = document.querySelector("#content")
@@ -7,6 +8,7 @@ btnGenerate.addEventListener("click", () => {
   const options= {
     margin: [10,10,10,10],
     filename: "nota.pdf",
+    image:        { type: 'jpeg', quality: 0.98 },
     html2canvas: {scale: 2},
     jsPDF: {unit: "mm" , format:"a4", orientation: "portrait"}
   }
@@ -15,23 +17,3 @@ btnGenerate.addEventListener("click", () => {
 
 
 });
-$(document).ready(function() {
-  // const valor1 = document.querySelector("#valor1")
-  // const valor2 = document.querySelector("#valor2")
-  // const valor3 = document.querySelector("#valor3")
-  // const valor4 = document.querySelector("#valor4")
-  // const valor5 = document.querySelector("#valor5")
-  // const valor6 = document.querySelector("#valor6")
-
-  function calcularResultado() {
-    // Obtenha os valores dos campos de entrada
-    var valorCampo1 = parseFloat(document.getElementById("valor1").value) || 0;
-    var valorCampo2 = parseFloat(document.getElementById("valor2").value) || 0;
-
-    // Realize o cálculo desejado (neste exemplo, a soma)
-    var resultado = valorCampo1 + valorCampo2;
-
-    // Atualize o valor do campo de resultado
-    document.querySelector("total").value = resultado;
-}
-})
